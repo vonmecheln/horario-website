4aadb53a2e4db82fbb463bc57b4a688273287e57
Luís Henrique Manosso Von Mecheln <luishenrique.si@gmail.com>
2021-08-13 14:39:22 -0300

    init commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,13 +5,13 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 module.exports = {
   title: 'IFPR Horário',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://vonmecheln.github.io/',
+  baseUrl: '/horario-website/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'vonmecheln', // Usually your GitHub org/user name.
+  projectName: 'horario-website', // Usually your repo name.
   themeConfig: {
     navbar: {
       title: 'IFPR Horário',
